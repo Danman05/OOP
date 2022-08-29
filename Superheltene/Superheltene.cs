ca0259ef8cba1788cd728849539f74d2325eaced
Danman05 <110387670+Danman05@users.noreply.github.com>
2022-08-29 11:47:29 +0200

    Add files via upload

---
+++ Superheltene/Superheltene.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superheltene
+{
+    class Superheltene
+    {  
+        // datatype string, Identity descriptions
+        public string maskStyle;
+        public string clothingStyle;
+        
+        public string colorOfMask;
+        public string colorOfClothing;
+
+        // datatype string, superPower
+        public string superPower;
+
+        // datatype string, moral
+        public string moral;
+
+        // datatype int, enemies
+        public int enemies;
+
+        // Method UseAttack
+        public string UseAttack()
+        {
+            // Metodekrop
+        }
+        // Method Fly
+        public string Fly() 
+        {
+            // Metodekrop
+        }        
+    }
+}
