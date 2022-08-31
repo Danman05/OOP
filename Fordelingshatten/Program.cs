6e019722ab3eff4795f46fc21848aef7fb9ca285
Danman05 <110387670+Danman05@users.noreply.github.com>
2022-08-31 10:04:36 +0200

    Add files via upload

---
+++ Fordelingshatten/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Fordelingshatten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortingHat chooseDorm = new SortingHat(); // Makes new object from class SortingHat 
+            Console.WriteLine(chooseDorm.DormChooser()); // Writes to console, the object and the objects method
+            Console.ReadLine(); // Readline to keep program running
+        }
+    }
+}
---
+++ Fordelingshatten/SortingHat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fordelingshatten
+{
+    class SortingHat
+    {
+        // Constructor
+        public SortingHat()
+        {
+            
+        }
+        // Method
+        public string DormChooser()
+        { 
+            Random rnd = new Random();
+            int random = rnd.Next(0, 4); // Random number between 0-3
+            string str = ((Dorms)random).ToString(); // Uses the random number to get the word from the enum, and makes the enum word toString
+            return str; // Returns str
+        }
+    }
+}
