5e0e8b1f44a92dcb2c133dbcfecec5cbf2178619
Danman05 <110387670+Danman05@users.noreply.github.com>
2022-09-01 19:55:25 +0200

    Add files via upload

---
+++ playlisten/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace playlisten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Playlist playlist = new Playlist(); // Creates playlist object
+
+            // Adds tracks to playlist
+            playlist.AddTrack("Lemon Tree");
+            playlist.AddTrack("Country Roads");
+
+            // Adds the track to first place in the linked list
+            playlist.AddTrackTop("Sweet dreams (Are Made Of This)");
+
+            // foreach loop through the linked list and write out each item in list
+            foreach (Track p in playlist.playlist)
+            {
+                Console.WriteLine(p.TrackName);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ playlisten/Track.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace playlisten
+{
+    class Track
+    {
+        // Field variables
+        private string trackName;
+
+        // properties
+        public string TrackName { get { return trackName; } }
+
+        // Constructor
+        public Track(string trackName)
+        {
+            this.trackName = trackName;
+        }
+    }
+}
