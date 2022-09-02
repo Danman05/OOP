12911c037c356c69e0c3d8fdb46ee66b6315d55e
Danman05 <110387670+Danman05@users.noreply.github.com>
2022-09-02 12:43:33 +0200

    Add files via upload

---
+++ Køen/Games.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Køen
+{
+    class Games         
+    {   // Field
+        private string gameTitle;
+
+        // properties
+        public string GameTitle { get { return gameTitle; } }
+
+        // Constructor
+        public Games(string gameTitle)
+        {
+            this.gameTitle = gameTitle;
+        }
+    }
+}
