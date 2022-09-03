70becf3063e7bf3f9e1204d5304e5bd9a7dac429
Danman05 <110387670+Danman05@users.noreply.github.com>
2022-09-03 21:41:44 +0200

    Add files via upload

---
+++ Jedi Mind Trick/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jedi_Mind_Trick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creates Dictionary
+            Dictionary<string, int> people = new Dictionary<string, int>();
+
+            //Adds "Jedi" at the age of 32
+            people.Add("Jedi", 50);
+            //Adds "Joda" at the age of 104
+            people.Add("Joda", 104);
+
+            //Writes each keyvaluepair in Dictionary people
+            foreach (KeyValuePair<string, int> item in people)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Creates Dictionary and add "luke, Han and Chewbacca"
+            Dictionary<string, bool> Characters = new Dictionary<string, bool>()
+            {
+                { "Luke", true},
+                { "Han", false},
+                {"ChewBacca", false}
+            };
+
+            //Removes "Han"
+            Characters.Remove("Han");
+
+            //Writes each KeyValuePair in Dictionary Characters
+            foreach (KeyValuePair<string, bool> item in Characters)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Keeps program alive
+            Console.ReadLine();
+        }
+    }
+}
